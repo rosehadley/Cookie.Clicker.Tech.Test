d4630edfc5c053947f6b6af72bf5a5f3d0530ca4
Rose Hadley <rhadley98@hotmail.com>
2026-07-28 17:26:15 +0100

    test: fix tests and skip tests with bugs

--- tests/view-scoreboard.spec.ts
+++ tests/view-scoreboard.spec.ts
@@ -3,29 +3,22 @@ import { test } from '../fixtures/base-test';
 
 test.describe('As a new user, the High Scores table is displayed correctly on the homepage', () => {
     let userOne: string, userTwo: string, userThree: string, userFour: string;
-    test.beforeAll('Create games with different scores', async ({ homePage, gamePage }) => {
+    test.beforeEach('Create games with different scores', async ({ homePage, gamePage }) => {
         userOne = await homePage.createNewGame();
         await gamePage.clickCookies(3);
-        
-        userTwo = await homePage.createNewGame();
-        await gamePage.clickCookies(10);
 
-        userThree = await homePage.createNewGame();
+        userTwo = await homePage.createNewGame();
         await gamePage.clickCookies(4);
 
-        await test.step('Create a user that can buy a factory with two cookies remaining', async () => {
-            userFour = await homePage.createNewGame();
-            await gamePage.clickCookies(14);
-            await gamePage.sellCookies(12); // Each cookie sells for $0.25
-            await gamePage.buyFactories(1); // Each factory costs $3
-        });
+        userThree = await homePage.createNewGame();
+        await gamePage.clickCookies(10);
 
         await homePage.goto();
     });
 
     test('As an existing user, I can navigate back to my game from the High Scores table on the homepage', { tag: ['@TC-008'] }, async ({ homePage, gamePage }) => {
         await test.step('Click on one of the user\'s names in the scoreboard', async () => {
-            await homePage.scoreTableUserName(userOne).click();
+            await homePage.scoreboardUserName(userOne).click();
 
             expect(gamePage.page, 'The user should be redirected to their game').toHaveURL(gamePage.url(userOne));
             expect(gamePage.gameGreeting(userOne), 'The game greeting should display the correct user\'s name').toBeVisible();
@@ -33,12 +26,12 @@ test.describe('As a new user, the High Scores table is displayed correctly on th
     });
 
     test('If a user has 0 factories and their score is static, their score in the High Scores table is correct', { tag: ['@TC-007'] }, async ({ homePage }) => {
-        await expect(homePage.scoreTableUserScore(userOne), 'User one should have a score of 3').toHaveText('3');
-        await expect(homePage.scoreTableUserScore(userTwo), 'User two should have a score of 10').toHaveText('10');
-        await expect(homePage.scoreTableUserScore(userThree), 'User three should have a score of 4').toHaveText('4');
+        await expect(homePage.scoreboardUserScore(userOne), 'User one should have a score of 3').toHaveText('3');
+        await expect(homePage.scoreboardUserScore(userTwo), 'User two should have a score of 10').toHaveText('10');
+        await expect(homePage.scoreboardUserScore(userThree), 'User three should have a score of 4').toHaveText('4');
     });
 
-    test('If a user has 0 factories and their score is static, the high scores are ordered by cookie count in descending order', { tag: ['@TC-003'], annotation: {
+    test.skip('If a user has 0 factories and their score is static, the high scores are ordered by cookie count in descending order', { tag: ['@TC-003'], annotation: {
         type: 'Bug',
         description: 'https://rhadley98s-team.monday.com/boards/5100988704/pulses/3119218901'
     }}, async ({ homePage }) => {
@@ -46,23 +39,32 @@ test.describe('As a new user, the High Scores table is displayed correctly on th
             const userOneScoreTablePosition = await homePage.getUserHighScoreTablePosition(userOne);
             const userTwoScoreTablePosition = await homePage.getUserHighScoreTablePosition(userTwo);
             const userThreeScoreTablePosition = await homePage.getUserHighScoreTablePosition(userThree);
+            console.log(userOneScoreTablePosition, userTwoScoreTablePosition, userThreeScoreTablePosition)
             
-            expect(userOneScoreTablePosition, 'User One should be below User Two on the scoreboard').toBeLessThan(userTwoScoreTablePosition);
-            expect(userOneScoreTablePosition, 'User One should be lower than User Three on the scoreboard').toBeLessThan(userThreeScoreTablePosition);
-            expect(userTwoScoreTablePosition, 'User Two should be higher than User Three on the scoreboard').toBeGreaterThan(userThreeScoreTablePosition);
+            expect.soft(userOneScoreTablePosition, 'User One should be below User Two on the scoreboard').toBeGreaterThan(userTwoScoreTablePosition);
+            expect.soft(userOneScoreTablePosition, 'User One should be below User Three on the scoreboard').toBeGreaterThan(userThreeScoreTablePosition);
+            expect.soft(userTwoScoreTablePosition, 'User Two should be below User Three on the scoreboard').toBeGreaterThan(userThreeScoreTablePosition);
         }); 
     });
 
-    test('If a user has factories and their score is not static, their score in the High Scores table is dynamically updated', { tag: ['@TC-007'], annotation: {
+    test.skip('If a user has factories and their score is not static, their score in the High Scores table is dynamically updated', { tag: ['@TC-007'], annotation: {
         type: 'Bug',
         description: 'https://rhadley98s-team.monday.com/boards/5100988704/pulses/3119185801'
-    } }, async ({ homePage }) => {
-        await test.step('Get the current score of the user, then wait 5 seconds and get their score again', async () => {        
-            const userFourScoreboardScore = Number(await homePage.scoreTableUserScore(userFour).innerText());
-            
-            await new Promise(resolve => setTimeout(resolve, 5000));
+    } }, async ({ gamePage, homePage }) => {
+        await test.step('Create a user that can buy a factory and have two cookies remaining', async () => {
+            userFour = await homePage.createNewGame();
+            await gamePage.clickCookies(14);
+            await gamePage.sellCookies(12); // Each cookie sells for $0.25
+            await gamePage.buyFactories(1); // Each factory costs $3
+            await homePage.goto();
+        });
 
-            const userFourScoreboardScoreAfter5Seconds = Number(await homePage.scoreTableUserScore(userFour).innerText());
+        await test.step('Get the current score of the user, then wait 3 seconds and get their score again', async () => {
+            const userFourScoreboardScore = Number(await homePage.scoreboardUserScore(userFour).textContent());
+            
+            await new Promise(resolve => setTimeout(resolve, 3000));
+
+            const userFourScoreboardScoreAfter5Seconds = Number(await homePage.scoreboardUserScore(userFour).textContent());
 
             expect(userFourScoreboardScoreAfter5Seconds, 'The user\'s score should have increased in 5 seconds').toBeGreaterThan(userFourScoreboardScore)
         });
